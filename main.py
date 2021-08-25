344e63bec16079bcc6372d733d53178b0bbd3766
MUTINTA <JustinMutinta@hotmail.com>
2021-08-25 12:01:08 -0500

    Tip Calculator, with comments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-print("Welcome to the tip Calculator")
-amount = input("What was the total bill? $")
-tip = input("What percentage tip would you like to tip? 10, 12, or 15? ")
-people = input("How many people to split the bill? ")
+print("Welcome to the tip Calculator")                                      #Welcoming print output
+amount = input("What was the total bill? $")                                #Get the total bill from the user
+tip = input("What percentage tip would you like to tip? 10, 12, or 15? ")   #Get the tip amount
+people = input("How many people to split the bill? ")                       #How many people were involved
 
-float_amount = float(amount)
-int_people = int(people)
-int_tip = (int(tip) / 100) * float_amount
+float_amount = float(amount)                                                #Change amount value to float for the decimal point
+int_people = int(people)                                                    #Change people to int
+int_tip = (int(tip) / 100) * float_amount                                   #Change tip to int
 
-result = round((float_amount + int_tip) / int_people , 2)
-print(f"Each person should pay: ${result}")
+result = round((float_amount + int_tip) / int_people , 2)                   #Calculate the result for each person
+print(f"Each person should pay: ${result}")                                 #Display Output
 
